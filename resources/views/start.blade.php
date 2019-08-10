a8824650585370c765300699f072e2a80a84fbd5
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-10 22:27:40 +0300

    appShape

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -19,7 +19,7 @@
 <div class="container-fluid" style="height: 100vh;">
 
     <div class="row">
-        <input id="btn_start" type="button" value="Load Data" onclick="start();">
+        <input id="btn_start" type="button" value="Load Data" onclick="Init.start();">
         <form id="myform" action="/guess" method="GET" style="display: none">
             <button type="submit">Next</button>
         </form>
@@ -27,80 +27,7 @@
 
 </div>
 
-<script>
-
-
-    function start() {
-        var csrf = document.getElementsByName('csrf-token')[0].getAttribute('content');
-        var shapes = {
-
-            0: {
-
-                "shape_name" : "square_1",
-                "shape_type_name": "square",
-                "params": {
-                    "x1": 100,
-                    "x2": 20
-                },
-                "extent": 240
-
-            },
-
-            1: {
-
-                "shape_name" : "square_2",
-                "shape_type_name": "square",
-                "params": {
-                    "x1": 150,
-                    "x2": 75
-                },
-                "extent": 450
-
-            }
-
-        };
-
-        var xhr = new XMLHttpRequest();
-        var data = {
-            shapes: shapes
-
-        };
-        xhr.open('POST','/',true);
-
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-CSRF-TOKEN", csrf);
-
-        xhr.onreadystatechange = function() {
-
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-
-                var res = JSON.parse(xhr.responseText || null);
-                var myform = document.getElementById('myform');
-                var btn_start = document.getElementById('btn_start');
-
-                if (res.data === "Error"){
-                    myform.style.display = "none";
-                    btn_start.style.display = "block";
-                    alert("Error with data");
-                }else{
-                    myform.style.display = "block";
-                    btn_start.style.display = "none";
-                    alert("Success press on Next button")
-                }
-
-            }
-
-        }
-
-        xhr.send(JSON.stringify(data));
-
-    }
-
-
-
-
-</script>
+<script src="{{asset('js/init_app.js')}}"></script>
 
 </body>
 </html>
